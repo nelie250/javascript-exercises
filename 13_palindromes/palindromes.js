830ca078b47583e78747ff2c21e96be8dedfc102
nelie250 <ntezielie@gmail.com>
2025-07-31 19:08:34 +0200

    feat: Implement solutions for JavaScript exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+  return  str.split('').reverse().join('') === str
 
 };
 
+
+console.log(palindromes('isi'))
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    if (n < 0) return "invalid number";
+    if (n == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+    
+    for (let i = 2; i <= n; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 
 };
-
+console.log(fibonacci(1))
 // Do not edit below this line
 module.exports = fibonacci;
